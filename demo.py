3ac4d4e8bf0efd98769fae520bdf2215234a3709
pravallika31792503 <pravallikakasula19@gmail.com>
2024-12-26 17:49:58 +0530

    all_resources

---
+++ demo.py
@@ -0,0 +1,8 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import sys
+
+from hate.configuration.gcloud_syncer import GCloudSync
+
+obj=GCloudSync()
+obj.sync_folder_from_gcloud("hate-speech2024_pk","dataset.zip","dataset.zip")
